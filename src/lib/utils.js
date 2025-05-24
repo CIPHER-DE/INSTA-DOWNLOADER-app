bd58bfbf463a60ce03f8ae9781a99e5e9a8c9d40
CIPHER-DE <donsavage20006@gmail.com>
2025-05-24 16:46:52 +0000

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,18 @@
 import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
+    import { twMerge } from "tailwind-merge"
+     
+    export function cn(...inputs) {
+      return twMerge(clsx(inputs))
+    }
+    
+    export const isValidInstagramUrl = (inputUrl) => {
+      try {
+        const parsedUrl = new URL(inputUrl);
+        return parsedUrl.hostname.includes('instagram.com') && 
+               (parsedUrl.pathname.includes('/reel/') || 
+                parsedUrl.pathname.includes('/p/') || 
+                parsedUrl.pathname.includes('/tv/'));
+      } catch (e) {
+        return false;
+      }
+    };
